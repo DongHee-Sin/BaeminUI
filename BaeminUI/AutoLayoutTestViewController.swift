f1cbfbae3500e3a1a5a761650e79fc61c63a5221
DongHee-Sin <voslr0601@naver.com>
2022-07-08 22:20:56 +0900

    메가박스 UI 추가

---
+++ BaeminUI/AutoLayoutTestViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AutoLayoutTestViewController.swift
+//  BaeminUI
+//
+//  Created by 신동희 on 2022/07/08.
+//
+
+import UIKit
+
+class AutoLayoutTestViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ BaeminUI/MegaboxViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MegaboxViewController.swift
+//  BaeminUI
+//
+//  Created by 신동희 on 2022/07/08.
+//
+
+import UIKit
+
+class MegaboxViewController: UIViewController {
+
+    
+    @IBOutlet weak var movieLisftView: UIView!
+    @IBOutlet weak var movieListStackView: UIStackView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addRadiusToUIViewTop(movieLisftView, radiusSize: movieLisftView.frame.width / 15)
+        
+        movieListStackView.subviews.forEach({
+            addShadow($0, color: UIColor.lightGray.cgColor, width: 5, height: 10, alpha: 0.5, radius: 15)
+            addRadiusToUIView($0, radiusSize: $0.frame.width / 10)
+        })
+    }
+
+    
+    func addRadiusToUIViewTop(_ view: UIView, radiusSize: CGFloat) {
+        view.layer.cornerRadius = radiusSize
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+    }
+    
+    func addRadiusToUIView(_ view: UIView, radiusSize: CGFloat) {
+        view.layer.cornerRadius = radiusSize
+    }
+    
+    func addShadow(_ to: UIView, color: CGColor, width: CGFloat, height: CGFloat, alpha: Float, radius: CGFloat) {
+        to.layer.shadowColor = color
+        to.layer.shadowOpacity = alpha
+        to.layer.shadowRadius = radius
+        to.layer.shadowOffset = CGSize(width: width, height: height)
+        to.layer.shadowPath = nil
+    }
+}
